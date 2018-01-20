4a331051f07ccfaad2b9092e26a3650ac7ad3a01
dcleung <derex.leung@gmail.com>
2018-01-20 17:37:23 -0500

    Integrate search

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Twitter = require('twitter');
 var credentials = require('./config.json');
 
-var MAX_COUNT = 30;
+var MAX_COUNT = 20;
  
 // Initialize the Twitter API Keys
 var client = new Twitter({
@@ -12,12 +12,14 @@ var client = new Twitter({
   access_token_key: credentials.access_token_key,
   access_token_secret: credentials.access_token_secret
 });
- 
-// Set the username in question
-var params = {screen_name: 'realDonaldTrump', count: MAX_COUNT, exclude_replies: true, include_rts: false, tweet_mode:'extended'};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    // Set the username in question
+    var params = {screen_name: req.query.q, count: MAX_COUNT, exclude_replies: true, include_rts: 0, tweet_mode:'extended'};
+    
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
         console.log(tweets);
