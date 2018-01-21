d23a9113366962c33dd732be2da15b57213a3ec8
dcleung <derex.leung@gmail.com>
2018-01-20 20:47:14 -0500

    Improve picture quality

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 var credentials = require('./config.json');
+var aws = require('aws-sdk');
+
+// Assign AWS Keys
+var aws_access_key = aws.aws_access_key;
+var aws_secret_key = aws.aws_access_key;
 
 var MAX_COUNT = 20;
  
@@ -22,12 +27,31 @@ router.get('/', function(req, res, next) {
     
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
-        console.log(tweets);
-        res.render('index', { tweets: tweets });
+        var retTweets = new Array();
+        tweets.forEach(function(tweet) {
+            tweet.full_text = convertText(tweet.full_text);
+            retTweets.push(tweet);
+        });
+
+        var newProfile = convertPic(tweets[0].user.profile_image_url);
+
+        res.render('index', { tweets: retTweets, profile: newProfile});
       } else {
         console.log(error);
       }
     });
 });
+
+// Make input text retro
+var convertText = function(text) {
+    return text;
+}
+
+// Make input picture retro
+var convertPic = function(pic) {
+    // Makes the twitter picture high quality
+    pic = pic.replace('_normal','');
+    return pic;
+}
 
 module.exports = router;
