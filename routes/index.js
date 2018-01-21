120078f0f0c7f85c45d0fe13716693ad93b54a0f
dcleung <derex.leung@gmail.com>
2018-01-21 00:15:24 -0500

    Composing images

--- routes/index.js
+++ routes/index.js
@@ -3,153 +3,11 @@ var router = express.Router();
 var Twitter = require('twitter');
 var credentials = require('./config.json');
 var AWS = require('aws-sdk');
+var Jimp = require("jimp");
 
 var request = require('request');
 
-var translate = {
-    "children" : "ankle biters",
-    "awesome" : "bad",
-    "sore throat" : "bad pipes",
-    "steal" : "bag",
-    "stole" : "bagged",
-    "study" : "bag some food for the brain",
-    "tricked" : "bearded",
-    "leave" : "beat it",
-    "grouchy" : "bent",
-    "knife" : "blade",
-    "event" : "blast",
-    "drunk" : "blitzed",
-    "body" : "bod",
-    "hurt" : "bogart",
-    "injured" : "bogart",
-    "failure" : "bomb",
-    "fantastic" : "bookin'",
-    "record player" : "box",
-    "money" : "bread",
-    "white socks" : "brights",
-    "brew" : "beer",
-    "to annoy" : "to bug",
-    "annoy" : "bug",
-    "annoyed" : "bugged",
-    "depressed" : "bummed out",
-    "fired" : "bumped",
-    "dismissed" : "bumped",
-    "cheated" : "burned",
-    "school lecture" : "chalk talk",
-    "lecture" : "chalk talk",
-    "ignore" : "chill",
-    "teeth" : "chops",
-    "roast" : "chop",
-    "tired" : "clanked",
-    "explain" : "clue you in",
-    "awkward person" : "clutz",
-    "jail" : "college",
-    "intoxicated" : "crocked",
-    "inebriated" : "crocked",
-    "to joke" : "cut up",
-    "Do you understand" : "Dig",
-    "idiot" : "ditz",
-    "peace lover" : "dove",
-    "nonconformist" : "dropout",
-    "rebel" : "dropout",
-    "geek" : "dude",
-    "boring" : "dullsville",
-    "roasted" : "ate dirt",
-    "attractive" : "fab",
-    "hot" : "fox",
-    "tattletale" : "fink",
-    "quit" : "fink out",
-    "talkative" : "flap jaw",
-    "movie" : "flick",
-    "movies" : "flicks",
-    "lose control" : "freak out",
-    "excellent" : "funky",
-    "police" : "fuzz",
-    "the best" : "gasser",
-    "handshake" : "gimme some skin",
-    "obstacle" : "glitch",
-    "cool" : "groovy",
-    "nice" : "groovy",
-    "money" : "green",
-    "disgusting" : "grody",
-    "a record" : "groove",
-    "records" : "grooves",
-    "record store" : "groove yard",
-    "neat" : "groovy",
-    "dirty" : "grungy",
-    "shabby" : "grungy",
-    "mad" : "hacked",
-    "problem" : "hang-up",
-    "Cadillac" : "hog",
-    "persuade" : "hustle",
-    "party" : "jam",
-    "excited" : "keyed",
-    "dismiss" : "kiss off",
-    "hobby" : "kicks",
-    "passtime" : "kicks",
-    "school" : "konk class",
-    "relaxed" : "laid back",
-    "pathetic" : "lame",
-    "sunbathe" : "lay out",
-    "freeloader" : "leaner",
-    "intoxicated" : "loaded",
-    "language" : "lingo",
-    "wrinkles" : "mileage",
-    "fine" : "mint",
-    "radio" : "music machine",
-    "jukebox" : "music machine",
-    "usefule" : "nifty",
-    "truth" : "nitty-gritty",
-    "now" : "fashionable",
-    "strange" : "off the wall",
-    "mom" : "old lady",
-    "mother" : "old lady",
-    "dad" : "old man",
-    "father" : "old man",
-    "crazy" : "out of his tree",
-    "unemployed" : "on the turf",
-    "responsible" : "on the hook",
-    "bed" : "pad",
-    "cheerleader" : "paper shaker",
-    "cop" : "pig",
-    "glasses" : "peppers",
-    "news" : "poop",
-    "sleep" : "rack",
-    "slept" : "racked",
-    "talk" : "rap",
-    "talked" : "rapped",
-    "clothes" : "rags",
-    "parents" : "rents",
-    "theft" : "rip off",
-    "fight" : "rumble", 
-    "spectacular" : "rockin' out",
-    "rejected" : "shot down",
-    "phony" : "shuck",
-    "fake" : "shuck",
-    "girl" : "skirt",
-    "sunglasses" : "shades",
-    "music" : "sounds",
-    "conceited" : "stuck up",
-    "loser" : "square",
-    "calm" : "stay lose",
-    "worry" : "sweat",
-    "teacher"  : "teach",
-    "friendly" : "tight",
-    "hitchhike" : "thumb",
-    "hurt" : "torn up",
-    "destroy" : "trash",
-    "never" : "Tuesday",
-    "songs" : "tunes",
-    "distraught" : "unglued",
-    "outstanding" : "unreal",
-    "nervous" : "uptight",
-    "open" : "upfront",
-    "cigarettes" : "weeds",
-    "car" : "wheels",
-    "pizza" : "za",
-    "zero" : "zilch",
-    "pimple" : "zit"
-}
+var translate = require('./dictionary.json')
 
 AWS.config = {
   "accessKeyId": credentials.aws_access_key,
@@ -160,6 +18,7 @@ AWS.config = {
 var s3 = new AWS.S3();
 var rekognition = new AWS.Rekognition();
 
+// Uploads the pic at the URL to S3
 function put_from_url(url, bucket, key, callback) {
     request({
         url: url,
@@ -177,6 +36,7 @@ function put_from_url(url, bucket, key, callback) {
     })
 }
 
+// Run Rekognition on the current picture
 function putCallback() {
     runRekognition({Image:{'S3Object':{'Bucket':'pennbook-my-images','Name':'current.png'}}});
 }
@@ -187,13 +47,64 @@ function runRekognition(searchParams) {
             console.log(err, err.stack); // an error occurred
         } else {
             console.log('Uploaded data successfully!'); 
-            console.log(data.FaceDetails[0].Landmarks);
+            replacePic(data);
         }
     });
 }
 
-function replace(data) {
+// Replace the current picture with the retro-fied picture
+function replacePic(data) {
+    //console.log(data.FaceDetails);
+    var leftEyeX = 0;
+    var leftEyeY = 0;
+    var rightEyeX = 0;
+    var rightEyeY = 0;
 
+    data.FaceDetails[0].Landmarks.forEach(function(landmark) {
+        if (landmark.Type === 'eyeLeft') {
+            leftEyeX = landmark.X;
+            leftEyeY = landmark.Y;
+        }
+
+        if (landmark.Type === 'eyeRight') {
+            rightEyeX = landmark.X;
+            rightEyeY = landmark.Y;
+        }
+    });
+
+    if (leftEyeX && leftEyeY && leftEyeX && leftEyeY) {
+
+        var newX = 50; 
+        var newY = 50;
+
+        // Compute where to put the afro image
+
+        // Have JIMP read the afro image
+        Jimp.read("http://pbs.twimg.com/profile_images/880285750872428546/nM435x8_.jpg", function (err, overlay) {
+            if (!err) {
+                // Have JIMP read the image for the Twitter profile
+                var params = {Bucket: 'pennbook-my-images', Key: 'current.png'};
+                var url = s3.getSignedUrl('getObject', params);
+                //console.log('The URL is', url);
+                Jimp.read(url, function (err, profile) {
+                    if (!err) {
+                        profile.composite(overlay, newX, newY); 
+                        var file = "./public/new." + profile.getExtension();
+                        profile.write(file)
+                        /*image.write(path, function(err) {
+                            if (err) console.log(err);
+                        });
+                        console.log("successful composite");*/
+                    } else {
+                        console.log(err);
+                    }
+                });
+            } else {
+                console.log(err);
+            }
+        });
+    }
+    console.log(data.FaceDetails[0].Landmarks);
 }
 
 // Maximum number of tweets to appear (keep small for testing purposes)
@@ -206,7 +117,6 @@ var client = new Twitter({
   access_token_key: credentials.access_token_key,
   access_token_secret: credentials.access_token_secret
 });
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -222,9 +132,9 @@ router.get('/', function(req, res, next) {
             retTweets.push(tweet);
         });
 
-        var newProfile = convertPic(tweets[0].user.profile_image_url);
+        let promise = convertPic(tweets[0].user.profile_image_url);
 
-        res.render('index', { tweets: retTweets, profile: newProfile});
+        res.render('index', { tweets: retTweets});
       } else {
         console.log(error);
       }
