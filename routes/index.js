3968d36860d934d03934810b9fabfc32f146bae7
dcleung <derex.leung@gmail.com>
2018-01-20 22:34:36 -0500

    Add S3 and Rekognition

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,56 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 var credentials = require('./config.json');
-var aws = require('aws-sdk');
+var AWS = require('aws-sdk');
 
-// Assign AWS Keys
-var aws_access_key = aws.aws_access_key;
-var aws_secret_key = aws.aws_access_key;
+var request = require('request');
 
+AWS.config = {
+  "accessKeyId": credentials.aws_access_key,
+  "secretAccessKey": credentials.aws_secret_key,
+  "region": "us-east-1"
+};
+
+var s3 = new AWS.S3();
+var rekognition = new AWS.Rekognition();
+
+function put_from_url(url, bucket, key, callback) {
+    request({
+        url: url,
+        encoding: null
+    }, function(err, res, body) {
+        if (err)
+            console.log(err);
+
+        s3.putObject({
+            Bucket: bucket,
+            Key: key,
+            Body: body,
+            ACL: 'public-read'
+        }, callback);
+    })
+}
+
+function putCallback() {
+    runRekognition({Image:{'S3Object':{'Bucket':'pennbook-my-images','Name':'current.png'}}});
+}
+
+function runRekognition(searchParams) {
+    rekognition.detectFaces(searchParams, function (err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+        } else {
+            console.log('Uploaded data successfully!'); 
+            console.log(data.FaceDetails[0].Landmarks);
+        }
+    });
+}
+
+function replace(data) {
+
+}
+
+// Maximum number of tweets to appear (keep small for testing purposes)
 var MAX_COUNT = 20;
  
 // Initialize the Twitter API Keys
@@ -22,7 +66,7 @@ var client = new Twitter({
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    // Set the username in question
+    // Set the Twitter handle in question
     var params = {screen_name: req.query.q, count: MAX_COUNT, exclude_replies: true, include_rts: 0, tweet_mode:'extended'};
     
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -51,6 +95,13 @@ var convertText = function(text) {
 var convertPic = function(pic) {
     // Makes the twitter picture high quality
     pic = pic.replace('_normal','');
+    put_from_url(pic, 'pennbook-my-images', 'current.png', function(err, res) {
+        if (err) {
+            throw err;
+        } else {
+            putCallback();
+        }
+    });
     return pic;
 }
 
